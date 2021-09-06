a369659252d4133dc9984b1ae8fc4afa99dfff3a
dustylpiper <dustylpiper@gmail.com>
2021-09-06 04:14:54 -0500

    Update for IMAP message listing
    Update to include pulling emails from imap mail box, populating email body. Note - Set credentials in dogpile.py (testing)

--- App/dogpile.py
+++ App/dogpile.py
@@ -1,7 +1,17 @@
-from flask import Flask, request, render_template, redirect, session, jsonify, json
+from flask import Flask, request, render_template, redirect, session, jsonify, json, send_from_directory
 import datetime
+import email
+import imaplib
+import mailbox
+import json
+import os
+import base64
 
 app = Flask(__name__,static_url_path='/static')
+
+imap_host = 'imap.gmail.com'
+imap_user = 'group10.dogpile.client@gmail.com'
+imap_pass = 'rwVE4a>[KSw9'
 
 @app.template_filter()
 def datetimefilter(value, format='%Y/%m/%d %H:%M'):
@@ -9,18 +19,87 @@ def datetimefilter(value, format='%Y/%m/%d %H:%M'):
 app.jinja_env.filters['datetimefilter'] = datetimefilter
 
 def getemaillist():
-  data = [
-             {"id":"1","name": "John", "email": "john@test.com", "subject":"We are trying to reach you about your warranty", "date": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M %p")},
-              {"id":"2","name": "Jenny", "email": "jenny@yahoo.com", "subject":"Can we just be friends?", "date": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M %p")},
-              {"id":"3","name": "Amanda", "email": "amanda@test.com", "subject":"Re: Going back to school", "date": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M %p")},
-              {"id":"4","name": "Matt", "email": "matt@test.com", "subject":"Data Request", "date": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M %p")},
-              {"id":"5","name": "Jacob", "email": "jacob@test.com", "subject":"Want to go to lunch?", "date": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M %p")},
-              {"id":"6","name": "Morgan", "email": "morgan@test.com", "subject":"Let us revolt against the boss!", "date": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M %p")},
-              {"id":"7","name": "Dusty", "email": "dusty@test.com", "subject":"Re: Let us revolt against the boss!", "date": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M %p")},
-              {"id":"8","name": "Carol", "email": "carol@test.com", "subject":"Here is my credit card info, as you requested", "date": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M %p")},
-              
-           ]
-  return (data)
+    mail_list = [] 
+    mail = imaplib.IMAP4_SSL(imap_host)
+    mail.login(imap_user, imap_pass)
+    mail.list()
+    mail.select('inbox')
+    result, data = mail.uid('search', None, "ALL") # (ALL/UNSEEN)
+    i = len(data[0].split())
+
+    for x in range(i):
+        latest_email_uid = data[0].split()[x]
+        result, email_data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+        # result, email_data = conn.store(num,'-FLAGS','\\Seen') 
+        # this might work to set flag to seen, if it doesn't already
+        raw_email = email_data[0][1]
+        raw_email_string = raw_email.decode('utf-8')
+        email_message = email.message_from_string(raw_email_string)
+        #print (email_message)
+        # Header Details
+        date_tuple = email.utils.parsedate_tz(email_message['Date'])
+        if date_tuple:
+            local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
+            local_message_date = "%s" %(str(local_date.strftime("%a, %m/%d/%Y, %H:%M %p")))
+        realname, addr = email.utils.parseaddr(email_message['from'])
+        email_to = str(email.header.make_header(email.header.decode_header(email_message['To'])))
+        subject = str(email.header.make_header(email.header.decode_header(email_message['Subject'])))
+        mail_item = {"uid": (latest_email_uid).decode("ascii"), "email_from_name":realname,"email_from_addr":addr, "email_to": email_to, "subject": subject, "date":local_message_date}
+        mail_list.append(mail_item)
+    else:
+        return (mail_list)
+def getMessage(m = None):
+    if m is not None:
+        mail_list = []
+        filename = None
+        mail = imaplib.IMAP4_SSL(imap_host)
+        mail.login(imap_user, imap_pass)
+        mail.list()
+        mail.select('inbox')
+        result, data = mail.uid('search', None, "ALL") # (ALL/UNSEEN)
+        mID = int(m)
+        latest_email_uid = data[0].split()[mID-1]
+        result, email_data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+        # result, email_data = conn.store(num,'-FLAGS','\\Seen') 
+        # this might work to set flag to seen, if it doesn't already
+        raw_email = email_data[0][1]
+        raw_email_string = raw_email.decode('utf-8')
+        email_message = email.message_from_string(raw_email_string)
+        #print (email_message)
+        # Header Details
+        for part in email_message.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            if part.get('Content-Disposition') is None:
+                continue
+            filename = part.get_filename()
+            att_path = os.path.join('attachments', filename)
+
+            if not os.path.isfile(att_path):
+                fp = open(att_path, 'wb')
+                fp.write(part.get_payload(decode=True))
+                fp.close()
+        if email_message.is_multipart():
+            for payload in email_message.get_payload():
+                for part in email_message.walk():
+                    if (part.get_content_type() == 'text/plain') and (part.get('Content-Disposition') is None):
+                        body = part.get_payload(decode=True)
+                break
+        else:
+            body = email_message.get_payload(decode=True)
+        realname, addr = email.utils.parseaddr(email_message['from'])
+        email_to = str(email.header.make_header(email.header.decode_header(email_message['To'])))
+        subject = str(email.header.make_header(email.header.decode_header(email_message['Subject'])))
+        date_tuple = email.utils.parsedate_tz(email_message['Date'])
+        if date_tuple:
+            local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
+            local_message_date = "%s" %(str(local_date.strftime("%a, %m/%d/%Y, %H:%M %p")))
+        mail_message = {"uid": (latest_email_uid).decode("ascii"), "email_from_name":realname,"email_from_addr":addr, "email_to": email_to, "subject": subject,
+         "date":local_message_date,"body":body.decode(), "attachments":filename}
+        mail_list.append(mail_message)
+        return (mail_list)
+    else:
+        return ("")
 def getConfig():
     configInfo = open("emailConfig.txt", 'r')
     data = {"host":configInfo.readline(),"port": configInfo.readline(), "protocol": configInfo.readline()}
@@ -35,9 +114,16 @@ def userAuth():
     userLen = len(userEmail) - userEmail.count('\n')
     passLen = len(userPassword) - userPassword.count('\n')
     if userLen >0 and passLen >0:
-      return True
+        return True
     else:
       return False
+
+@app.route('/attachments/<path:filename>')
+def getFile(filename):
+    try:
+        return send_from_directory('attachments', filename, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -82,17 +168,7 @@ def config():
 def home(idname = None):
     if userAuth() == False:
         return redirect("/")
-    
-    return render_template('_inbox.html', reply = None, activemessage = idname, data=getemaillist(), email_body = message(idname), config = getConfig())
-def message(fname):
-  if fname is None:
-    return "No Mail!"
-  if fname:
-    return """<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore.</p>
-<p>Magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in.</p>
-<p>Voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat. Non proident sunt.</p>
-<p>In culpa qui officia deserunt mollit anim id est laborum sed ut perspiciatis unde omnis iste natus error.</p>
-<p>Sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae. Dicta sunt explicabo.</p>"""
+    return render_template('_inbox.html', reply = None, activemessage = idname, data=getemaillist(), email_body = getMessage(idname), config = getConfig())
 
 @app.route("/inbox/<idname>/reply")
 @app.route("/inbox/<idname>/forward")
@@ -100,21 +176,8 @@ def message(fname):
 def homereply(idname = None):
     if userAuth() == False:
         return redirect("/")
-    listdata = getemaillist()
-    message = [x for x in listdata if x['id'] == idname]
-    #return (json.dumps(message[0]))
-    return render_template('_inbox.html', reply = idname, activemessage = None, data=listdata, email = message[0]["email"],email_body = messagereply(message[0]), config = getConfig())
-def messagereply(mData):
-  if mData is None:
-    return "No Mail!"
-  if mData:
-    body = "Subject: " +mData["subject"]+"<br>"+"From: "+mData["email"]+"<br>"+"Date: "+mData["date"]+"<br>"
-    body += """<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore.</p>
-<p>Magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in.</p>
-<p>Voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat. Non proident sunt.</p>
-<p>In culpa qui officia deserunt mollit anim id est laborum sed ut perspiciatis unde omnis iste natus error.</p>
-<p>Sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae. Dicta sunt explicabo.</p>"""
-    return (body)
+    return render_template('_inbox.html', reply = idname, activemessage = None, data=getemaillist(), email_body = getMessage(idname), config = getConfig())
+
 @app.route("/send", methods=['POST'])
 def send():
     formTo = request.form['To']
@@ -125,4 +188,3 @@ def send():
     message += formFrom + "<br>"
     message += formBody
     return (message)
-
