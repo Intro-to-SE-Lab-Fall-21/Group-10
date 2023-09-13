e2227a64d4ef66ffb8d687f82b4b483af4e60239
SouperNova97 <143524122+SouperNova97@users.noreply.github.com>
2023-09-12 21:03:46 -0500

    Update app.py

--- App/app.py
+++ App/app.py
@@ -686,4 +686,4 @@ def test_email_config_authenticated_post():
         assert response.status_code == 302
 	
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000,debug=False)
+    app.run(host='0.0.0.0', port=80,debug=False)
